7cb2cf26dbb26cd0a3f298cb7559dbab73740e88
Devon Campbell <raddevon@gmail.com>
2013-05-14 16:18:57 -0400

    Initial commit

---
+++ sass/config.rb
@@ -0,0 +1,9 @@
+require "susy"
+http_path = "/"
+css_dir = "../css"
+sass_dir = "."
+images_dir = "../img"
+javascripts_dir = "../js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
