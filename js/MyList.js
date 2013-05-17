c308cbf5c87e124d3421c566bcc8b5debeeeb6c7
Devon Campbell <raddevon@gmail.com>
2013-05-17 13:21:19 -0400

    Separated list class from list interface functions

---
+++ js/MyList.js
@@ -0,0 +1,22 @@
+function MyList() {
+    // A user list
+
+    function openExisting() {
+        if (supports_html5_storage()) {
+            this.theList = localStorage.getItem('theList');
+        } else {
+            $.cookie.json = true;
+            this.theList = $.cookie('the_list');
+        }
+    }
+
+    function saveList() {
+        // todo Create the JSON object for the list and store in this.theList
+        this.theListString = JSON.stringify(this.theList);
+        if (supports_html5_storage()) {
+            localStorage.setItem('theList', this.theListString);
+        } else {
+            $.cookie('the_list', this.theListString);
+        }
+    }
+}
--- js/list.js
+++ js/list.js
@@ -1,29 +1,10 @@
-function MyList() {
-    // A user list
-
-    function openExisting() {
-        if (supports_html5_storage()) {
-            this.theList = localStorage.getItem('theList');
-        } else {
-            $.cookie.json = true;
-            this.theList = $.cookie('the_list');
-        }
-    }
-
-    function saveList() {
-        // todo Create the JSON object for the list and store in this.theList
-        this.theListString = JSON.stringify(this.theList);
-        if (supports_html5_storage()) {
-            localStorage.setItem('theList', this.theListString);
-        } else {
-            $.cookie('the_list', this.theListString);
-        }
-    }
-
-    function newItem() {
-        var item = $('<li><form action=""><input type="text" /></form></li>')
-        if ($('.list li').length > 0) {
-
-        }
+function newItem() {
+    var item = $('<li class="new-item"><form action=""><input type="text" /></form></li>');
+    if ($('.list li').length === 0) {
+        item.prependTo($('.list ul')).removeClass('new');
+    } else {
+        $('.list li').css('top', $('.list li').outerHeight());
+        item.prependTo($('.list ul')).removeClass('new');
+        $('.list li').css('top', 0);
     }
 }
