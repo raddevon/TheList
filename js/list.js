0f8dcd3f60a3e20c18e921a747b03b444abcbbe0
Devon Campbell <raddevon@gmail.com>
2013-05-19 17:41:07 -0400

    New list item function

--- js/list.js
+++ js/list.js
@@ -1,10 +1,14 @@
-function newItem() {
-    var item = $('<li class="new-item"><form action=""><input type="text" /></form></li>');
-    if ($('.list li').length === 0) {
-        item.prependTo($('.list ul')).removeClass('new');
-    } else {
-        $('.list li').css('top', $('.list li').outerHeight());
-        item.prependTo($('.list ul')).removeClass('new');
-        $('.list li').css('top', 0);
-    }
-}
+(function( $ ){
+    $.fn.newItem = function() {
+        $(this).bind('click', function() {
+            var item = $('<li class="new-item"><form action=""><input type="text" /></form></li>');
+            if ($('.list li').length === 0 && $('.list li').first().val()) {
+                item.prependTo($('.list ul')).removeClass('new');
+            } else {
+                $('.list li').css('top', $('.list li').outerHeight());
+                item.prependTo($('.list ul')).removeClass('new');
+                $('.list li').css('top', 0);
+            }
+        });
+    };
+})( jQuery );
